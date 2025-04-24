1a17c5b29e46e91c9c7a909049f93e1d8404b995
n8allan <3095770+n8allan@users.noreply.github.com>
2025-04-24 15:16:41 -0600

    Removed dist junk

--- dist/test/example.spec.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'aegir/chai';
-describe('Example', () => {
-    it('should be true', () => {
-        expect(true).to.equal(true);
-    });
-});
-//# sourceMappingURL=example.spec.js.map
