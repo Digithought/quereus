de6891ec9ccbc24ca2d2db20ce541aca6e00012a
Nathan Allan <nate@digithought.com>
2026-01-31 23:59:41 -0700

    storage-fs in wrong repo

--- packages/db-p2p-storage-fs/src/logger.ts
+++
@@ -1,8 +0,0 @@
-import debug from 'debug';
-
-const BASE_NAMESPACE = 'optimystic:db-p2p-storage-fs';
-
-export function createLogger(subNamespace: string): debug.Debugger {
-	return debug(`${BASE_NAMESPACE}:${subNamespace}`);
-}
-
